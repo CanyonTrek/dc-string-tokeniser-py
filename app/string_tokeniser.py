c2826834971234b61cc05a24ee88895fea93dbbd
Donald Cameron <donald.cameron@qa.com>
2025-06-23 12:25:29 +0100

    Added app, tests and main.py for v1.0

---
+++ app/string_tokeniser.py
@@ -0,0 +1,4 @@
+class StringTokeniser:
+    def tokenise(self, input_val: str) -> list[str]:
+        result = []
+        return result
---
+++ tests/test_string_tokeniser.py
@@ -0,0 +1,22 @@
+import unittest
+from app.string_tokeniser import StringTokeniser
+
+class TestStringTokeniser(unittest.TestCase):
+
+    def test_empty_string_result_empty_array(self):
+        # If this was the only requirement, 
+        # then the CUT as coded would be sufficient
+        # arrange
+        input_val = ""
+        cut = StringTokeniser()
+        expected_result = []
+
+        # act
+        actual_result = cut.tokenise(input_val)
+
+        # assert
+        self.assertEqual(expected_result, actual_result)
+
+if __name__ == "__main__":
+    unittest.main()
+
